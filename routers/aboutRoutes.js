45ffd856bcdc4a9e07825918895aa21d546548ef
bahodir373 <zteqwertyuiop672@gmail.com>
2025-02-26 21:13:15 +0500

    i think it will work

--- routers/aboutRoutes.js
+++ routers/aboutRoutes.js
@@ -39,13 +39,13 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, uploadDir);
-    },
-    filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, uniqueSuffix + path.extname(file.originalname));
-    }
+  destination: (req, file, cb) => {
+    cb(null, "uploads/"); 
+  },
+  filename: (req, file, cb) => {
+    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1E9);
+    cb(null, uniqueName + path.extname(file.originalname));
+  }
 });
 const upload = multer({ storage });
 
